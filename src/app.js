ea4bdc57ff8cf2d6a154b65b630fbc8b2895ba4f
sik <sik@simcorp.com>
2018-04-27 23:24:44 +0300

    side

--- src/app.js
+++ src/app.js
@@ -1,2 +1,24 @@
 import './assets/scss/app.scss';
-console.log('Its working just fine');
+
+import $ from 'jquery';
+
+$(function () {
+
+    var $win = $(window);
+    var $body = $('body');
+    var targetEement = $('#caption');
+    var targetHeight = targetEement.offset().top + targetEement.outerHeight();
+
+    $('.menu-toggle').on('click', function () {
+        $body.toggleClass('menu-open');
+    });
+
+    $win.on("scroll", function () {
+        if ($win.scrollTop() > targetHeight) {
+            $body.removeClass("menu-open");
+        } else {
+            $body.addClass("menu-open");
+        }
+    });
+
+});
